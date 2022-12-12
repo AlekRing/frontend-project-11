1e18747e1038220beeab95611373ad21358898b5
alekRing <okolcov60@gmail.com>
2022-12-12 11:41:28 +0300

    fix hexlet linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,57 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	devtool: 'inline-source-map',
-	mode: process.env.NODE_ENV || 'development',
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/',
-	},
-	resolve: {
-		extensions: ['.js'],
-		alias: {
-			'@': path.resolve(__dirname, 'src/'),
-		},
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js?$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
-				},
-			},
-			{ test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-			{
-				test: /\.scss$/,
-				use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-			},
-			{
-				test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-				use: 'url-loader?limit=10000',
-			},
-			{
-				test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-				use: 'file-loader',
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './dist/index.html',
-		}),
-	],
-	devServer: {
-		static: path.join(__dirname, 'dist'),
-		compress: true,
-		port: process.env.PORT || 3000,
-		historyApiFallback: true,
-		// open: true,
-	},
+  devtool: 'inline-source-map',
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './dist/index.html',
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: process.env.PORT || 3000,
+    historyApiFallback: true,
+    // open: true,
+  },
 };
